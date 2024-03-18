5097375e078155bf9086bfd950be9dd97687c682
Seandowling123 <dowling.sean103@gmail.com>
2024-03-18 19:34:45 +0000

    plots for tictac toe done

--- connect4_game.py
+++ connect4_game.py
@@ -762,10 +762,10 @@ human = Human_player()
 rand = Random_player()
 minimax = Minimax_player()
 #playa1 = Q_learning_player(policy_name="Connect_Four_Q_learning_agent")
-qlearning = Q_learning_player(policy_name="Connect_four_Q_learning_agents/Connect_Four_Q_learning_agent10000")
+qlearning = Q_learning_player(policy_name="Connect_four_Q_learning_agents/Connect_Four_Q_learning_agent30000")
 #qlearning.train_Qlearning_agent(100001)
 
 #test_Q_learning_agents(qlearning, default, 1000)
 #test_agents(minimax, default, 1000)
 
-play_connect_four(tictactoe_board, qlearning, default)
+play_connect_four(tictactoe_board, default, qlearning)
