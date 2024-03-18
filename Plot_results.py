2ffbccb10fa4fde884d0c1c7bcd18c8f08656cdc
Seandowling123 <dowling.sean103@gmail.com>
2024-03-18 19:38:04 +0000

    ok plots done now

--- Plot_results.py
+++ Plot_results.py
@@ -14,7 +14,7 @@ colors = ['#27ae60', '#e74c3c', '#2980b9']
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.size'] = 10
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-plt.title('Win Ratios of Minimax & Default Player with Minimax As Player 1', fontsize=12, fontfamily='serif')
+plt.title('Win Ratios of Minimax Vs Default Player with Minimax As Player 1', fontsize=12, fontfamily='serif')
 plt.axis('equal')
 plt.savefig('Plots/Minimax_P1_vs_Default.png', bbox_inches='tight')
 #plt.show()
@@ -25,7 +25,7 @@ labels = ['Ties','Minimax wins','Default Player wins']
 colors = ['#27ae60', '#e74c3c', '#2980b9']
 
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-plt.title('Win Ratios of Minimax & Default Player with Minimax As Player 2', fontsize=12, fontfamily='serif')
+plt.title('Win Ratios of Minimax Vs Default Player with Minimax As Player 2', fontsize=12, fontfamily='serif')
 plt.axis('equal')
 plt.savefig('Plots/Minimax_P2_vs_Default.png', bbox_inches='tight')
 #plt.show()
