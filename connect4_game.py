cb0877eddba2bab16e21b61cbbdf14cfe175687f
Seandowling123 <dowling.sean103@gmail.com>
2024-03-19 15:17:04 +0000

    removed 30 inutes stuff

--- connect4_game.py
+++ connect4_game.py
@@ -5,12 +5,6 @@ import pickle
 import math
 import random
 import csv
-import time
-
-explored_moves = 0
-total_moves = 0
-start_time = 0
-time_limit = 30 * 60
 
 # Place the counter at the bottom of the selected column
 def get_placement(board, index):
@@ -355,23 +349,18 @@ class Minimax_player:
         global total_moves
         global start_time
         global time_limit
-        explored_moves + 1
-        total_moves + len(available_moves)
-        
-        current_time = time.time()
-        print(current_time - start_time, time_limit)
-        if (current_time - start_time) > time_limit:
-            return [0,0], 0
+        explored_moves = explored_moves + 1
+        total_moves = total_moves + len(available_moves)
         
         # Check if the game is over
         if board.result() != None:
             return self.get_state_reward(board)
         
-        """# If max depth is reached, check heuristics
+        # If max depth is reached, check heuristics
         if depth == self.depth:
             if self.is_player_1:
                 return None, get_state_heuristic(board)
-            else: return None, (-1)*get_state_heuristic(board)"""
+            else: return None, (-1)*get_state_heuristic(board)
                 
         # Calculate move for maximiser
         if is_maximising:
@@ -425,13 +414,8 @@ class Minimax_player:
          
     # Return the next move for the player
     def get_next_move(self, board):
-        global start_time
-        global explored_moves
-        global total_moves
-        start_time = time.time()
         move = self.minimax(board, -math.inf, math.inf, True, 0)
         self.played_moves = self.played_moves+1
-        print(explored_moves, total_moves)
         return get_placement(board, move[0])
     
 class Q_learning_player:
